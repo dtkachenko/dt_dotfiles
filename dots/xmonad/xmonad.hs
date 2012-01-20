d63e9a0a27acadb9e7f1542b5e692f4990f69bcc
Dmitry Tkachenko <sepulci@gmail.com>
2012-01-21 02:38:38 +0400

    add xmobar support to xmonad

--- dots/xmonad/xmonad.hs
+++ dots/xmonad/xmonad.hs
@@ -3,6 +3,10 @@ import XMonad.Config.Gnome
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import System.IO
+import XMonad.Util.Run
 
 myManageHook = composeAll (
     [ manageHook gnomeConfig
@@ -15,11 +19,17 @@ myManageHook = composeAll (
 
 myModMask       = mod4Mask
 
-main = xmonad $ gnomeConfig
-        { manageHook = myManageHook,
-          modMask    = myModMask
-        }
-        `additionalKeysP`
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = myModMask
+        } `additionalKeysP`
                   [ ("M-p",  spawn "gmrun")
                   ]
 
